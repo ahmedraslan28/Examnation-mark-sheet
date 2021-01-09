e2265a971d414c1263a19f593bdeaaa509eb9160
ahmedraslan28 <48288103+ahmedraslan28@users.noreply.github.com>
2021-01-09 13:47:32 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace StudentSheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tdata = new System.Windows.Forms.TextBox();
+            this.tmath = new System.Windows.Forms.TextBox();
+            this.tname = new System.Windows.Forms.TextBox();
+            this.toop = new System.Windows.Forms.TextBox();
+            this.teng = new System.Windows.Forms.TextBox();
+            this.tacc = new System.Windows.Forms.TextBox();
+            this.tlogic = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tsec = new System.Windows.Forms.TextBox();
+            this.tid = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 97);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(898, 147);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Math";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(867, 67);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Data Structure";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(671, 67);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "OOP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(465, 67);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "English";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(657, 147);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Accountiog";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(447, 147);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Logic Design";
+            // 
+            // tdata
+            // 
+            this.tdata.Location = new System.Drawing.Point(890, 86);
+            this.tdata.Name = "tdata";
+            this.tdata.Size = new System.Drawing.Size(63, 22);
+            this.tdata.TabIndex = 14;
+            // 
+            // tmath
+            // 
+            this.tmath.Location = new System.Drawing.Point(890, 166);
+            this.tmath.Name = "tmath";
+            this.tmath.Size = new System.Drawing.Size(63, 22);
+            this.tmath.TabIndex = 15;
+            // 
+            // tname
+            // 
+            this.tname.Location = new System.Drawing.Point(82, 12);
+            this.tname.Name = "tname";
+            this.tname.Size = new System.Drawing.Size(265, 22);
+            this.tname.TabIndex = 16;
+            this.tname.TextChanged += new System.EventHandler(this.tname_TextChanged);
+            // 
+            // toop
+            // 
+            this.toop.Location = new System.Drawing.Point(660, 86);
+            this.toop.Name = "toop";
+            this.toop.Size = new System.Drawing.Size(63, 22);
+            this.toop.TabIndex = 17;
+            // 
+            // teng
+            // 
+            this.teng.Location = new System.Drawing.Point(461, 86);
+            this.teng.Name = "teng";
+            this.teng.Size = new System.Drawing.Size(63, 22);
+            this.teng.TabIndex = 18;
+            this.teng.TextChanged += new System.EventHandler(this.teng_TextChanged);
+            // 
+            // tacc
+            // 
+            this.tacc.Location = new System.Drawing.Point(660, 166);
+            this.tacc.Name = "tacc";
+            this.tacc.Size = new System.Drawing.Size(63, 22);
+            this.tacc.TabIndex = 19;
+            // 
+            // tlogic
+            // 
+            this.tlogic.Location = new System.Drawing.Point(461, 166);
+            this.tlogic.Name = "tlogic";
+            this.tlogic.Size = new System.Drawing.Size(63, 22);
+            this.tlogic.TabIndex = 20;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 196);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1176, 331);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 21;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 147);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 42);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Delete Row";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 97);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 42);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(229, 147);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 42);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Clear List";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Sec";
+            // 
+            // tsec
+            // 
+            this.tsec.Location = new System.Drawing.Point(82, 40);
+            this.tsec.Name = "tsec";
+            this.tsec.Size = new System.Drawing.Size(265, 22);
+            this.tsec.TabIndex = 26;
+            this.tsec.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // tid
+            // 
+            this.tid.Location = new System.Drawing.Point(82, 68);
+            this.tid.Name = "tid";
+            this.tid.Size = new System.Drawing.Size(265, 22);
+            this.tid.TabIndex = 28;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 74);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 16);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "ID";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(279, 97);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(68, 42);
+            this.button5.TabIndex = 29;
+            this.button5.Text = "Empty Cells";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(522, 12);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(279, 16);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "The Marks should be between (100 && 0)";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1200, 554);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.tid);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tsec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.tlogic);
+            this.Controls.Add(this.tacc);
+            this.Controls.Add(this.teng);
+            this.Controls.Add(this.toop);
+            this.Controls.Add(this.tname);
+            this.Controls.Add(this.tmath);
+            this.Controls.Add(this.tdata);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Examination Mark Sheet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tdata;
+        private System.Windows.Forms.TextBox tmath;
+        private System.Windows.Forms.TextBox tname;
+        private System.Windows.Forms.TextBox toop;
+        private System.Windows.Forms.TextBox teng;
+        private System.Windows.Forms.TextBox tacc;
+        private System.Windows.Forms.TextBox tlogic;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tsec;
+        private System.Windows.Forms.TextBox tid;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentSheet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Name", 300 , HorizontalAlignment.Left);
+            listView1.Columns.Add("ID", 100, HorizontalAlignment.Left);
+            listView1.Columns.Add("Sec", 60, HorizontalAlignment.Left);
+            listView1.Columns.Add("Eng", 60, HorizontalAlignment.Center);
+            listView1.Columns.Add("Math", 60, HorizontalAlignment.Center);
+            listView1.Columns.Add("logic", 60, HorizontalAlignment.Center);
+            listView1.Columns.Add("DS", 60, HorizontalAlignment.Center);
+            listView1.Columns.Add("Acc", 60, HorizontalAlignment.Center);
+            listView1.Columns.Add("OOP", 60, HorizontalAlignment.Center);
+            listView1.Columns.Add("Percentage", 100 , HorizontalAlignment.Center);
+            listView1.Columns.Add("gpa", 60, HorizontalAlignment.Center);
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double eng, mat, log, ds, acc, opp, tot1 ;
+            string gpa = "" ; 
+            eng = Convert.ToDouble(teng.Text);
+            mat = Convert.ToDouble(tmath.Text);
+            log = Convert.ToDouble(tlogic.Text);
+            ds = Convert.ToDouble(tdata.Text);
+            acc = Convert.ToDouble(tacc.Text);
+            opp = Convert.ToDouble(toop.Text);
+            tot1 = ( (eng + mat + log + ds + acc + opp) / (600) ) *100;
+            double tot = Math.Round(tot1, 2);
+            bool found = true ; 
+            if(tot > 100 || 0 > tot)
+            {
+                MessageBox.Show("Invlaid marks please chek it again");
+                found = false ;
+            }
+            else if (100 >= tot && tot >= 90) gpa = "A+";
+            else if (90 > tot && tot >= 85) gpa = "A";
+            else if (85 > tot && tot >= 80) gpa = "B";
+            else if (80 > tot && tot >= 70) gpa = "C";
+            else if (70 > tot && tot >= 50) gpa = "D";
+            else gpa = "F";
+            if (found == true)
+            {
+                string a = Convert.ToString(tot);
+                string[] res = { tname.Text, tid.Text, tsec.Text, teng.Text, tmath.Text, tlogic.Text, tdata.Text, tacc.Text, toop.Text, a + "%", gpa };
+                ListViewItem item = new ListViewItem(res);
+                listView1.Items.Add(item);
+                ClEAR();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView1.SelectedItems[0].SubItems[0].Text = tname.Text ;
+            listView1.SelectedItems[0].SubItems[1].Text = tid.Text;
+            listView1.SelectedItems[0].SubItems[2].Text = tsec.Text;
+            listView1.SelectedItems[0].SubItems[3].Text = teng.Text;
+            listView1.SelectedItems[0].SubItems[4].Text = tmath.Text;
+            listView1.SelectedItems[0].SubItems[5].Text = tlogic.Text;
+            listView1.SelectedItems[0].SubItems[6].Text = tdata.Text;
+            listView1.SelectedItems[0].SubItems[7].Text = tacc.Text;
+            listView1.SelectedItems[0].SubItems[8].Text = toop.Text;
+            double eng, mat, log, ds, acc, opp, tot1;
+            string gpa = "" ;
+            eng = Convert.ToDouble(teng.Text);
+            mat = Convert.ToDouble(tmath.Text);
+            log = Convert.ToDouble(tlogic.Text);
+            ds = Convert.ToDouble(tdata.Text);
+            acc = Convert.ToDouble(tacc.Text);
+            opp = Convert.ToDouble(toop.Text);
+            tot1 = ((eng + mat + log + ds + acc + opp) / (600)) * 100;
+            double tot = Math.Round(tot1, 2);
+            bool found = true; 
+            if (tot > 100 || 0 > tot)
+            {
+                MessageBox.Show("Invlaid marks please chek it again");
+                found = false ; 
+            }
+            else if (100 >= tot && tot >= 90) gpa = "A+";
+            else if (90 > tot && tot >= 85) gpa = "A";
+            else if (85 > tot && tot >= 80) gpa = "B";
+            else if (80 > tot && tot >= 70) gpa = "C";
+            else if (70 > tot && tot >= 50) gpa = "D";
+            else gpa = "F";
+            if(found == true)
+            {
+                string a = Convert.ToString(tot);
+                listView1.SelectedItems[0].SubItems[9].Text = a;
+                listView1.SelectedItems[0].SubItems[10].Text = gpa;
+            }
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void teng_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void ClEAR()
+        {
+            tname.Clear();
+            tsec.Clear();
+            tmath.Clear();
+            teng.Clear();
+            tlogic.Clear();
+            toop.Clear();
+            tdata.Clear();
+            tacc.Clear();
+            tid.Clear();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ClEAR();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are You Sure ???", "Delete", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) != DialogResult.Cancel)
+            {
+                listView1.Items.RemoveAt(listView1.SelectedIndices[0]);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            tname.Text = listView1.SelectedItems[0].SubItems[0].Text;
+            tid.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            tsec.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            teng.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            tmath.Text = listView1.SelectedItems[0].SubItems[4].Text;
+            tlogic.Text = listView1.SelectedItems[0].SubItems[5].Text;
+            tdata.Text = listView1.SelectedItems[0].SubItems[6].Text;
+            tacc.Text = listView1.SelectedItems[0].SubItems[7].Text;
+            toop.Text = listView1.SelectedItems[0].SubItems[8].Text;
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
